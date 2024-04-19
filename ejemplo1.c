d76ee858e1e0525da555f1941c00a7236075695d
axellct <axellact@uamv.edu.ni>
2024-04-18 18:57:27 -0600

    Agrego el ejercicio

---
+++ ejemplo1.c
@@ -0,0 +1,19 @@
+/*sumar dos numeros*/
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+
+{
+    int num1;
+    int num2;
+    int suma;
+    printf("Dame los numeros que deseas sumar");
+    scanf("%i", &num1, );
+    scanf("%i", &num2);
+    suma = num1 + num2;
+
+    printf("la suma de los numeros es %i ,%i",suma);
+
+    return 0;
+}
